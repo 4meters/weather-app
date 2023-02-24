a0d5eef8e4ffabf877bd7d49d02552ef27934059
root <root@raspberrypi>
2023-02-24 13:27:56 +0100

    Local version rpi

--- src/ServerURL.js
+++ src/ServerURL.js
@@ -1,5 +1,7 @@
 //Here you can change server URL which uses client app
 //
 
-export const BASE_SERVER_URL = "https://weather-serverapplication.herokuapp.com"
+//export const BASE_SERVER_URL = "https://weather-serverapplication.herokuapp.com"
 //export const BASE_SERVER_URL = "http://127.0.0.1:8080"
+export const BASE_SERVER_URL = "http://192.168.1.68:8080"
+

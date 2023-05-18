9d59fd4d9479679e423d9ff3e02a7f958d988dd0
Marcin Ryznar <mryznar@onet.pl>
2023-05-18 19:19:04 +0200

    Change server host to fly.io

--- src/ServerURL.js
+++ src/ServerURL.js
@@ -1,5 +1,6 @@
 //Here you can change server URL which uses client app
 //
 
-export const BASE_SERVER_URL = "https://weather-serverapplication.herokuapp.com"
+export const BASE_SERVER_URL = "https://weather-server.fly.dev/"
+//export const BASE_SERVER_URL = "https://weather-serverapplication.herokuapp.com"
 //export const BASE_SERVER_URL = "http://127.0.0.1:8080"

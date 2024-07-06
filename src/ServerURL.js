6a1b900de32ee9857342b7e020b46d89938fb09f
Marcin Ryznar <mryznar@onet.pl>
2024-07-07 01:59:45 +0200

    update ServerURL to Railway

--- src/ServerURL.js
+++ src/ServerURL.js
@@ -1,6 +1,7 @@
 //Here you can change server URL which uses client app
 //
 
-export const BASE_SERVER_URL = "https://weather-server.fly.dev"
+export const BASE_SERVER_URL = "weather-server-production-7a67.up.railway.app"
+//export const BASE_SERVER_URL = "https://weather-server.fly.dev"
 //export const BASE_SERVER_URL = "https://weather-serverapplication.herokuapp.com"
 //export const BASE_SERVER_URL = "http://127.0.0.1:8080"
